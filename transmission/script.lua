2a83f0945f73900a9b177bd3e384abb6c0b59596
codelover <626973633@qq.com>
2023-08-19 23:48:31 +0800

    feat: update

--- transmission/script.lua
+++ transmission/script.lua
@@ -35,10 +35,52 @@ local global = {
     }
 }
 
-function getState(status)
-
+function getStartArg(id)
+    return {
+        arguments = {
+            ids = id,
+        },
+        method = "torrent-start"
+    }
 end
 
+function getStopArg(id)
+    return {
+        arguments = {
+            ids = id,
+        },
+        method = "torrent-stop"
+    }
+end
+
+function getDeleteArg(id)
+    return {
+        arguments = {
+            ids = id,
+        },
+        method = "torrent-remove"
+    }
+end
+
+function getDeleteFileArg(id)
+    return {
+        arguments = {
+            ids = id,
+            ["delete-local-data"] = true,
+        },
+        method = "torrent-remove"
+    }
+end
+
+function getDownloadFileArg(url,path)
+    return {
+        arguments = {
+            filename = url,
+            ["download-dir"] = path,
+        },
+        method = "torrent-add"
+    }
+end
 
 function doRequest(method,url,data)
     req = http.request(method,url,json.encode(data))
@@ -71,7 +113,6 @@ local function NewTransmission(ctx)
         runCtx = ctx.ctx     -- 运行上下文
     }
 
-
     function self:Update()
         local app = NewApp()
         local url = string.format(global.urlFormat,self.config.HostPort)
@@ -81,6 +122,16 @@ local function NewTransmission(ctx)
         local fontSize = 10
         for i = 1, #data.arguments.torrents do
             local d = data.arguments.torrents[i]
+            local downloadAndUpload = ""
+            if tonumber(d.rateDownload) > 0 then
+                downloadAndUpload =  string.format("↓%s",ByteToUiString(d.rateDownload))
+            end
+            if tonumber(d.rateUpload) > 0 then
+                downloadAndUpload = downloadAndUpload..string.format(" ↑%s",ByteToUiString(d.rateUpload))
+            end
+            if downloadAndUpload == "" then
+                downloadAndUpload =  string.format("↓%s",ByteToUiString(d.rateDownload))
+            end
             local line = NewProcessLineUi()
                     .SetDesc(
                         NewText("leading")
@@ -91,7 +142,7 @@ local function NewTransmission(ctx)
                                             .SetBackendColor("#F00")
                                             .SetColor("#FFF"))
                                 .AddString(2,
-                                NewString(string.format("↓%s/S ↑%s/S", ByteToUiString(d.rateDownload), ByteToUiString(d.rateUpload)))
+                                NewString(downloadAndUpload)
                                         .SetFontSize(8)
                                         .SetBackendColor("#66cccc")
                                         .SetColor("#FFF"))
@@ -101,7 +152,6 @@ local function NewTransmission(ctx)
                                         .SetBackendColor("#66cccc")
                                         .SetColor("#FFF"))
 
-
                     )
                     .SetTitle(NewText("trailing")
                     .AddString(1,
@@ -109,14 +159,59 @@ local function NewTransmission(ctx)
                             .SetFontSize(8)
                             .SetOpacity(0.5)
                     )
+
                     )
                     .SetProcessData(NewProcessData(d.percentDone*100,100))
+
+            if d.status == 0 then
+                line.AddAction(NewAction("start",{id=d.id},"继续"))
+            else
+                line.AddAction(NewAction("stop",{id=d.id},"暂停"))
+            end
+            line.AddAction(NewAction("delete",{id=d.id},"删除").SetCheck(true))
+                .AddAction(NewAction("deleteFile",{id=d.id},"删除并清理文件").SetCheck(true))
             app.AddUi(index,line)
+
+
             if i%2 == 0 then
                 index = index+1
             end
         end
+        local buttonSize = 17
+        app.AddMenu(NewIconButton()
+                .SetSize(buttonSize)
+                .SetIcon("plus.circle")
+                .SetAction(NewAction("download",{},"")
+                    .AddInput("Path",NewInput("路径",1).SetVal(self.config.DownloadPath))
+                    .AddInput("Url",NewInput("下载链接",2))
+                )
+        )
         return app.Data()
+    end
+
+    function self:Start()
+        local url = string.format(global.urlFormat,self.config.HostPort)
+        doRequest("POST",url,getStartArg(self.arg.id))
+    end
+
+    function self:Stop()
+        local url = string.format(global.urlFormat,self.config.HostPort)
+        doRequest("POST",url,getStopArg(self.arg.id))
+    end
+
+    function self:Delete()
+        local url = string.format(global.urlFormat,self.config.HostPort)
+        doRequest("POST",url,getDeleteArg(self.arg.id))
+    end
+
+    function self:DeleteFile()
+        local url = string.format(global.urlFormat,self.config.HostPort)
+        doRequest("POST",url,getDeleteFileArg(self.arg.id))
+    end
+
+    function self:Download()
+        local url = string.format(global.urlFormat,self.config.HostPort)
+        doRequest("POST",url,getDownloadFileArg(self.input.Url,self.input.Path))
     end
 
     return self
@@ -132,4 +227,26 @@ end
 ---@return AppUIData
 function update(ctx)
     return NewTransmission(ctx):Update()
+end
+
+
+function start(ctx)
+    return NewTransmission(ctx):Start()
+end
+
+function stop(ctx)
+    return NewTransmission(ctx):Stop()
+end
+
+function delete(ctx)
+    return NewTransmission(ctx):Delete()
+end
+
+function deleteFile(ctx)
+    return NewTransmission(ctx):DeleteFile()
+end
+
+
+function  download(ctx)
+    return NewTransmission(ctx):Download()
 end
