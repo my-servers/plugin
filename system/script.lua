00bbfd037e5de7c857eb0764e0aca4c790474701
codelover <626973633@qq.com>
2023-08-29 23:45:53 +0800

    feat: update

--- system/script.lua
+++ system/script.lua
@@ -65,9 +65,11 @@ local function NewSystem(ctx)
     local function getMemDetail(m)
         local detail = string.format([[
 ### 内存
+
 - 总内存： `%s`
 - 可用：`%s`
 - 已使用：`%s`
+
 ]],ByteToUiString(m.Total),
             ByteToUiString(m.Available),
              ByteToUiString(m.Used)
