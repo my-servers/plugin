08fb5625e483ab3d9223f323137dd225330b3a29
kevenqiu <kevenqiu@tencent.com>
2023-11-24 01:17:02 +0800

    feat: 优化安装脚本

--- auto_install.sh
+++ auto_install.sh
@@ -18,9 +18,38 @@ if [[ "$app_dir" == "" ]]; then
 fi
 
 if ! [ -d "$app_dir" ]; then
-  mkdir ~/.myservers
+  mkdir $app_dir
 fi
 
+apps_dir=$app_dir"/app"
+if ! [ -d "$apps_dir" ]; then
+  mkdir -p $apps_dir
+  for item in ${app_dir}/*; do
+  if [ "${item}" != "$apps_dir" ]; then
+    mv "${item}" "${apps_dir}/"
+  fi
+done
+fi
+
+config_dir=$app_dir"/config"
+if ! [ -d "$config_dir" ]; then
+  mkdir $config_dir
+  cat > ${config_dir}"/config.yaml" << "EOF"
+RestConfig:
+  Name: MyServers
+  Host: 0.0.0.0
+  Port: 18612
+  Log:
+    Stat: false
+    Level: error
+SecretKey: e8edf0cd4c5d49694c39edf7a879a92e
+PluginUrl: https://plugin.codeloverme.cn/
+MarkdownPage:
+  About: https://plugin.codeloverme.cn/about.md
+AppDir: /app/apps
+Name: MyServers
+EOF
+fi
 # 检查密钥长度是否为32
 if [ ${#secret_key} -ne 32 ]; then
     # 判断系统类型，选择合适的md5命令
@@ -35,13 +64,14 @@ fi
 
 # 拉取Docker镜像（替换为你的Docker镜像名称）
 docker pull myservers/my_servers
-# 运行Docker容器（替换为你的Docker镜像名称和需要的环境变量）
-docker run -d --network=host -v $app_dir:/apps -e AppDir=/apps -e SecretKey=$secret_key --name myServers --restart=always myservers/my_servers
+# # 运行Docker容器（替换为你的Docker镜像名称和需要的环境变量）
+docker run -d --network=host -v ${apps_dir}:/app/apps -v ${config_dir}:/app/config --name myServers --restart=always myservers/my_servers /app/app -k $secret_key -c /app/config/config.yaml
 # 输出运行状态
 
 clear
 docker ps | grep myServers
 echo -e "服务器程序已成功部署！"
-echo "密钥：$secret_key"
-echo "插件目录：$app_dir"
-echo "端口：18612"
+echo "密钥: $secret_key"
+echo "插件目录: $apps_dir"
+echo "配置文件目录: $config_dir"
+echo "端口: 18612"
